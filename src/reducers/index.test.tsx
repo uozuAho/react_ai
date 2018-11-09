e00d168ff64e69097e2da49e8f6d7ba1e9599d2b
Warwick Stone <uozu.aho@gmail.com>
2018-11-09 21:51:20 +1100

    add store, actions and reducers

---
+++ src/reducers/index.test.tsx
@@ -0,0 +1,27 @@
+import { enthusiasm } from '.';
+import { decrementEnthusiasm, incrementEnthusiasm } from '../actions';
+import { IStoreState } from "../types";
+
+it('should increment enthusiasm', () => {
+    const state = {languageName: '', enthusiasmLevel: 1} as IStoreState;
+
+    const nextState = enthusiasm(state, incrementEnthusiasm());
+
+    expect(nextState.enthusiasmLevel).toBe(2);
+});
+
+it('should decrement enthusiasm', () => {
+    const state = {languageName: '', enthusiasmLevel: 2} as IStoreState;
+
+    const nextState = enthusiasm(state, decrementEnthusiasm());
+
+    expect(nextState.enthusiasmLevel).toBe(1);
+});
+
+it('should not decrement enthusiasm past 1', () => {
+    const state = {languageName: '', enthusiasmLevel: 1} as IStoreState;
+
+    const nextState = enthusiasm(state, decrementEnthusiasm());
+
+    expect(nextState.enthusiasmLevel).toBe(1);
+});
