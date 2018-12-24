a08e106fdaab56f32c6f1adc450c0332074f9b0f
Warwick Stone <uozu.aho@gmail.com>
2018-12-24 20:10:42 +1100

    sidebar works good

--- src/react_ui/components/SidebarRouter.tsx
+++ src/react_ui/components/SidebarRouter.tsx
@@ -2,62 +2,75 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Hello } from './Hello';
 
-interface IRouteItem {
-  path: string;
-  render: () => JSX.Element;
+interface ISidebarItem {
+  route: string;
   exact: boolean;
+  sidebarText: string,
+  render: () => JSX.Element;
 }
 
-class RouteItem implements IRouteItem {
+class SidebarItem implements ISidebarItem {
+
+  public exact = false;
+
   constructor(
-    public path: string,
-    public render: () => JSX.Element,
-    public exact = false
+    public route: string,
+    public sidebarText: string,
+    public render: () => JSX.Element
   ) {}
+
+  public matchRouteExactly(): SidebarItem {
+    this.exact = true;
+    return this;
+  }
 }
 
-const routes: IRouteItem[] = [
-  new RouteItem("/",
-    () => <h2>No one's home</h2>,
-    true
-  ),
-  new RouteItem(
+const sidebarItems: ISidebarItem[] = [
+  new SidebarItem(
+    "/",
+    "Home",
+    () => <h2>No one's home</h2>
+  ).matchRouteExactly(),
+  new SidebarItem(
     "/hello",
+    "Hello",
     () => <Hello />
   ),
-  new RouteItem(
+  new SidebarItem(
     "/shoelaces",
+    "Shoelaces",
     () => <h2>Shoelacesoo</h2>
   )
 ];
 
-const renderSidebar = () => (
-  <div
-    style={{
-      padding: "10px",
-      width: "40%",
-    }}
-  >
-    <ul style={{ listStyleType: "none", padding: 0 }}>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/hello">Hello</Link>
-      </li>
-      <li>
-        <Link to="/shoelaces">Shoelaces</Link>
-      </li>
-    </ul>
-  </div>
-)
+const renderSidebar = (items: ISidebarItem[]) => {
+
+  const sidebarLinks = items.map(item =>
+    <li key={item.sidebarText}>
+      <Link to={item.route}>{item.sidebarText}</Link>
+    </li>
+  );
+
+  return (
+    <div
+      style={{
+        padding: "10px",
+        width: "40%",
+      }}
+    >
+      <ul>
+        {sidebarLinks}
+      </ul>
+    </div>
+  );
+}
 
 const renderContent = () => (
   <div style={{ flex: 1, padding: "10px" }}>
-    {routes.map((route, index) => (
+    {sidebarItems.map((route, index) => (
       <Route
         key={index}
-        path={route.path}
+        path={route.route}
         exact={route.exact}
         component={route.render}
       />
@@ -68,7 +81,7 @@ const renderContent = () => (
 export const SidebarRouter = () => (
   <Router>
       <div style={{ display: "flex" }}>
-        {renderSidebar()}
+        {renderSidebar(sidebarItems)}
         {renderContent()}
       </div>
   </Router>
