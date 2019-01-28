5f5429f9b268877cdb63d66372e42f4e12fe24b7
Warwick Stone <uozu.aho@gmail.com>
2019-01-28 16:25:01 +1100

    add min and max to iter utils

--- src/libs/array/iter_utils.ts
+++ src/libs/array/iter_utils.ts
@@ -10,3 +10,32 @@ export function firstOrNull<T>(itr: Iterable<T>, predicate: (t: T) => boolean): 
     }
     return null;
 }
+
+export function min<T>(itr: Iterable<T>, compare?: (a: T, b: T) => number): T {
+    const comparer = compare ? compare : defaultCompare;
+    return chooseWithChooser(itr, (a, b) => comparer(a, b) < 0 ? a : b);
+}
+
+export function max<T>(itr: Iterable<T>, compare?: (a: T, b: T) => number): T {
+    const comparer = compare ? compare : defaultCompare;
+    return chooseWithChooser(itr, (a, b) => comparer(a, b) < 0 ? b : a);
+}
+
+function chooseWithChooser<T>(itr: Iterable<T>, choose: (a: T, b: T) => T): T {
+    let current: T | null = null;
+
+    for (const item of itr) {
+        if (current === null) {
+            current = item;
+        }
+        current = choose(current, item);
+    }
+    if (current === null) {
+        throw new Error('empty iterable');
+    }
+    return current;
+}
+
+function defaultCompare<T>(a: T, b: T) {
+    return a < b ? -1 : a > b ? 1 : 0;
+}
